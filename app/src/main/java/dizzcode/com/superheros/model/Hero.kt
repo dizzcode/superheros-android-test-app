800c384f98b43ed7ee0762be8c3005d74fdbcfda
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-16 22:19:35 +0530

    Add fonts

---
+++ app/src/main/java/dizzcode/com/superheros/model/Hero.kt
@@ -0,0 +1,10 @@
+package dizzcode.com.superheros.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
